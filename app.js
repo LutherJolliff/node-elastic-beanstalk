919ba92b326574a2bf6443d1ed8d7d031581557d
Luther <luther@autostructure.io>
2019-08-06 11:44:58 -0400

    Added buildspec file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('Sample Node.js App');
+    res.send('Sample Node.js App WHICH IS NOW AWESOME');
 });
 
 app.listen(port, function () {
